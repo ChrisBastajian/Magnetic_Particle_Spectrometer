13e14a5918fee1e89f118b36fa04857393a8558b
MPS_App <chrisbastajian@gmail.com>
2025-03-31 19:04:01 -0400

    File Dropdown Added

--- MPS_app.py
+++ MPS_app.py
@@ -6,6 +6,7 @@ import receive_and_analyze as analyze
 from tkinter import filedialog, Toplevel, StringVar
 from scipy.io import savemat
 from matplotlib.figure import Figure
+from tkinter import filedialog, Toplevel, StringVar, Scrollbar, Listbox
 
 #For the live frequency array:
 import wave_gen
@@ -29,7 +30,40 @@ class App(ctk.CTk):
 
         self.geometry(str(width) + "x" + str(height))
         self.configure(fg="#333333")
+########################################################################################################################
+        #Setting up elements in places:
+        #Title Bar:
+        self.title_bar = ctk.CTkFrame(self, height=height//36, width=width, corner_radius=0, fg_color='gray') #height about 30 px
+        self.title_bar.place(x= width//2, y=height//72, anchor="center")
 
+        self.title_bar.file = ctk.CTkButton(self.title_bar, text="File", command=self.open_dropdown)
+        self.title_bar.file.place(x= 0.03*width, y= height//72, anchor='center')
+        self.title_bar.file.configure(width=0.06*width)
+
+
+    def open_dropdown(self):
+        # Create a new Toplevel window for the dropdown
+        dropdown_window = ctk.CTkToplevel(self)
+        dropdown_window.title("Select Option")
+        dropdown_window.geometry("200x300")
+
+        # Create a frame for scrollable content
+        frame = ctk.CTkFrame(dropdown_window)
+        frame.pack(fill="both", expand=True)
+
+        # Create a scrollbar
+        scrollbar = ctk.CTkScrollbar(frame)
+        scrollbar.pack(side="right", fill="y")
+
+        # Create a listbox with options (using standard Tkinter Listbox)
+        listbox = Listbox(frame, height=6, yscrollcommand=scrollbar.set)
+        options = ['File', 'Save Results', 'Plot']
+        for option in options:
+            listbox.insert("end", option)
+        listbox.pack(fill="both", expand=True)
+
+        # Configure the scrollbar
+        scrollbar.configure(command=listbox.yview)
 
 if __name__ == "__main__":
     app = App()
