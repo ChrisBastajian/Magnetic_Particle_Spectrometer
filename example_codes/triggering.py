37d10164d7bb6999ab9c2fb6614bace31a9eadf9
MPS_App <chrisbastajian@gmail.com>
2025-04-25 13:12:03 -0400

    added triggering

--- example_codes/triggering.py
+++ example_codes/triggering.py
@@ -2,6 +2,7 @@ from nidaqmx.constants import Edge
 import matplotlib.pyplot as plt
 import wave_gen
 import nidaqmx
+import receive_and_analyze as analyze
 
 #Signal Parameters:
 gpib_address = 10
@@ -23,22 +24,11 @@ wave_gen.send_voltage(waveform_generator, voltage, f, channel)
 num_pts_per_period = fs/f
 num_samples = int(num_pts_per_period*num_periods)
 
-def receive_voltage(daq_location, sample_rate, n_samps, trigger_location=None,):
-    with nidaqmx.Task() as task:
-        task.ai_channels.add_ai_voltage_chan(daq_location)
-        # Add a trigger source and set it to rising edge
-        if trigger_location is not None:
-            task.triggers.start_trigger.cfg_dig_edge_start_trig(trigger_location, Edge.RISING)
-
-        task.timing.cfg_samp_clk_timing(sample_rate, samps_per_chan=n_samps)
-        voltage_raw = task.read(number_of_samples_per_channel= n_samps)
-        return voltage_raw
-
 
 plt.figure()
 plt.subplot(2,1,1)
 for _ in range(5):
-    voltage = receive_voltage(sig_chan, fs, num_samples)
+    voltage = analyze.receive_raw_voltage(sig_chan, fs, num_samples)
     plt.plot(voltage, alpha=0.6)
 
 plt.title("Overlay of  Non-triggered Waveforms")
@@ -48,7 +38,7 @@ plt.grid(True)
 
 plt.subplot(2,1, 2)
 for _ in range(5):
-    voltage = receive_voltage(sig_chan, fs, num_samples, trigger_location=trig_chan)
+    voltage = analyze.receive_raw_voltage(sig_chan, fs, num_samples, trigger_location=trig_chan)
     plt.plot(voltage, alpha=0.6)
 
 plt.title("Overlay of Triggered Waveforms")
